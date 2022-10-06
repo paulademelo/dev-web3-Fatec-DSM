271b71646f733dec7c1700c9ce11abf93aa59c2d
paulademelo <56200258+paulademelo@users.noreply.github.com>
2022-10-05 22:07:50 -0300

    validando se o campo está válido

--- django/feriados/core/views.py
+++ django/feriados/core/views.py
@@ -1,8 +1,8 @@
-
 from asyncore import dispatcher_with_send
 from django.shortcuts import render
 from datetime import datetime
 from .models import FeriadoModel
+from .forms import FeriadoForm
 
 def verifica_feriado(request):
     hoje = datetime.today()
@@ -12,3 +12,20 @@ def verifica_feriado(request):
     else:
         contexto = {'feriado': False}
     return render(request, 'feriado.html', contexto)
+
+def cadastrar(request):
+    if request.method == 'POST':
+        # método para gravar
+        form = FeriadoForm(request.POST)
+        if form.is_valid():
+            contexto = {}
+            return render(request, 'Feriado.html', contexto)
+        else:
+            contexto = {'form', form}
+            return render(request, 'Feriado.html', contexto)
+
+    else:
+        # fluxo para exibir o formulário vazio GET
+        form = FeriadoForm()
+        contexto = {'form' : form}
+        return render(request, 'cadastrar.hmtl', contexto)
